e85049f7db994c979d2f25507783eccf84e48fba
unknown <TSPLuchev23@codingburgas.bg>
2025-11-15 23:18:14 +0200

    Maze Generation

--- project/src/main/main/main.cpp
+++ project/src/main/main/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include "valuce.h"
 #include "generateM.h"
+
 using namespace std;
 
 int main()
 {
-    int N; //size ot the maze
-    do {
-        cout << "Input size of the Labyrinth (10,20,30,40,50):";
-        cin >> N;
+    cout << "enter maze size: ";
+    cin >> mazeSize;
 
-        if ((N == 10) || (N == 20) || (N == 30) || (N == 40) || (N == 50)) {
-            int L[50][50];
-            FindPathInMaze(N, L);
-            DrowMaze(N, L);
-        }
-    } while ((N == 10) || (N == 20) || (N == 30) || (N == 40) || (N == 50));
 
+    GenerateMazeDFS(mazeSize, maze);
+
+    drawGame(maze, playerX, playerY, mazeSize, showDeadEnds);
+
+    
 }
